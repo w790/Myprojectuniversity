ee0109e81d8a5576c16a3201040c2f678d71fc7e
ksa16 <ksa16@tpu.ru>
2024-12-21 17:22:50 +0700

    Добавил своё приветствие

--- Myprojectuniversity/Program.cs
+++ Myprojectuniversity/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Привет,  8B42!");
 Console.WriteLine("Привет from kovin!");
+Console.WriteLine("Привет from ksa16!");
