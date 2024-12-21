ff2dbe1e4708e96c8a17da12a720ea963b035022
Roman Kovin <kovinrv@tpu.ru>
2024-12-21 17:26:07 +0700

    change comment

--- Myprojectuniversity/Program.cs
+++ Myprojectuniversity/Program.cs
@@ -1,3 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Привет,  8B42!");
-Console.WriteLine("Привет from kovin!");
+Console.WriteLine("Привет from kovin!"); // kovin
